b959f683f7efb69c2f6fa0757da3a4202029810d
tstep916 <tstep916@tamu.edu>
2019-02-19 13:39:33 -0600

    Added material ui.

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,10 +1,13 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Button from "@material-ui/core/Button";
+
 class HelloWorld extends React.Component {
   render () {
     return (
       <React.Fragment>
         Hello {this.props.name}
+        <Button color="inherit">Press Me</Button>
       </React.Fragment>
     );
   }
