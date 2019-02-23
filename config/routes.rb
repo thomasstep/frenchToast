dbf545b4a8ac8615c7486e5af9c8349bcbdd37e2
tstep916 <tstep916@tamu.edu>
2019-02-23 10:10:25 -0600

    Kind of a form.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
-  
+
   get 'services', to: 'welcome#services'
   get '/about_us', to: 'welcome#about_us'
+  get 'schedule', to: 'welcome#schedule'
 end
