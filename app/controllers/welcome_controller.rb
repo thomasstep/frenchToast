9f01fcfc68807643de4e80ee7c6de8d5229c4fc2
Christopher Sherman <csherman1998@tamu.edu>
2019-03-05 19:07:23 -0600

    basic appointment addition to table with validation check

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -13,11 +13,29 @@ class WelcomeController < ApplicationController
   end
 
   def schedule
+
     @schedule = Appointment.all
-    @firstName = params[:firstName]
-    @lastName = params[:lastName]
-    print @firstName
-    print @lastName
+    #wanna see the most disgusting code ever?
+
+    @email = params[:email]
+    @phone = params[:phone]
+    @vehicleYear = params[:vehicleYear]
+    @vehicleMake = params[:vehicleMake]
+    @vehicleModel = params[:vehicleModel]
+    @vehicleVin = params[:vehicleVin]
+    @desiredDate = params[:desiredDate]
+    @desiredTime = params[:desiredTime]
+    @reason = params[:reason]
+    
+    app = Appointment.new(owner_email: @email, VIN: @vehicleVin, date: @desiredDate,time: @desiredTime, reason: @reason, )
+    if app.valid? 
+        app.save
+    else
+      #do something, maybe flash an error
+    end
+    
+    print 
+
   end
   
   def my_profile
