6b0090ffba7044e337c47720399c33b8dcf5555e
tstep916 <tstep916@tamu.edu>
2019-04-09 10:06:04 -0500

    Refactored the scheduling page.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,7 +1,42 @@
 class AppointmentsController < ApplicationController
 
   def new
+    @user_first_name = ""
+    @user_last_name = ""
+    @user_email = ""
+    @user_phone = ""
+    if user_signed_in?
+      if !current_user.email.nil?
+        @user_email = current_user.email
+      end
+      if !current_user.first_name.nil?
+        @user_first_name = current_user.first_name
+      end
+      if !current_user.last_name.nil?
+        @user_last_name = current_user.last_name
+      end
+      if !current_user.phone.nil?
+        @user_phone = current_user.phone
+      end
+    end
     @appointment = Appointment.new
+  end
+
+  def create
+    if appointment_params[:time] == 1
+      appointment_params[:time] = "Morning"
+    elsif appointment_params[:time] == 2
+      appointment_params[:time] = "Afternoon"
+    elsif appointment_params[:time] == 3
+      appointment_params[:time] = "Evening"
+    end
+    
+    @appointment = Appointment.new(appointment_params)
+    if @appointment.save
+      redirect_to '/my_profile'
+    else
+      render :new
+    end
   end
 
   def edit
@@ -19,7 +54,7 @@ class AppointmentsController < ApplicationController
   end
 
   def appointment_params
-    params.require(:appointment).permit(:first_name)
+    params.require(:appointment).permit(:first_name, :last_name, :owner_email, :phone, :year, :make, :model, :VIN, :time, :date, :reason)
   end
 
   def update_appointment_params
