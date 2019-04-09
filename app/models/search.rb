013e4dc87cc59fd07110b710cf1a6bb73372b146
aj-najjar <aj.najjar@tamu.edu>
2019-04-09 21:02:59 +0000

    removed debugging comment

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,7 +2,7 @@ class Search < ApplicationRecord
     
     def search_appointments
         appointments = Appointment.all
-        appointments = appointments.where('"VIN" = ?', self.VIN) if self.VIN.present? #Gives an error when trying to filter using VIN...
+        appointments = appointments.where('"VIN" = ?', self.VIN) if self.VIN.present?
         appointments = appointments.where("owner_email LIKE ?", email) if email.present?
         appointments = appointments.where("date >= ?", after_date) if after_date.present?
         appointments = appointments.where("date <= ?", before_date) if before_date.present?
