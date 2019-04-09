dbfb1e34ea02ffc0963bed75a120dc6711a83567
tstep916 <tstep916@tamu.edu>
2019-04-09 17:59:35 -0500

    Change cucumber test to work. Took out my_garage. Took out unnecessary rspec tests from welcome_controller.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -21,31 +21,6 @@ class WelcomeController < ApplicationController
     end
   end
 
-  def my_garage
-  end
-
-  def new_car
-    @addcar_cancelled = false
-    @addedcar = false
-
-    #wanna see the most disgusting code ever?
-    if params.has_key?(:vehicleVin)
-      @vehicleYear = params[:vehicleYear]
-      @vehicleMake = params[:vehicleMake]
-      @vehicleModel = params[:vehicleModel]
-      @vehicleVin = params[:vehicleVin]
-
-      @new = Car.new(email: current_user.email, year: @vehicleYear, VIN: @vehicleVin, make: @vehicleMake, model: @vehicleModel)
-      if @new.valid?
-        @addedcar = true
-        @new.save
-      else
-        flash[:notice] = "Unable to add car"
-        @addcar_cancelled = true
-      end
-    end
-  end
-
   def admin
     if(current_user.nil? or !current_user.admin?)
       redirect_to "/not_admin"
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -22,45 +22,4 @@ RSpec.describe WelcomeController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
-
-  describe "Check that the new_car page only makes a new car if the necessary information is provided" do
-    it "should not allow a new car addition while missing the VIN" do
-      user = User.new(
-          :email => "test@gmail.com",
-          :password => "test1234",
-          :phone => "5555551230",
-          :first_name => "John",
-          :last_name => "Test"
-        )
-      user.save!
-      sign_in user
-      controller.params[:vehicleYear] = "2001"
-      controller.params[:vehicleMake] = "Ford"
-      controller.params[:vehicleModel] = "Model T"
-      controller.params[:vehicleVin] = ""
-      controller.new_car
-      expect(assigns(:addcar_cancelled)).to eq(true)
-    end
-  end
-
-  describe "Check that cars can be created if fields are properly filled out" do
-    it "should be able to delete create a car" do
-      user = User.new(
-          :email => "test@gmail.com",
-          :password => "test1234",
-          :phone => "5555551230",
-          :first_name => "John",
-          :last_name => "Test"
-        )
-      user.save!
-      sign_in user
-      controller.params[:vehicleYear] = "2001"
-      controller.params[:vehicleMake] = "Ford"
-      controller.params[:vehicleModel] = "Model T"
-      controller.params[:vehicleVin] = "00000000000000001"
-      controller.new_car
-      expect(Car.find_by_model("Model T").nil?).to eq(false)
-    end
-  end
-
 end
