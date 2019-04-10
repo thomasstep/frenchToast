d8dd8373ab13a447674b157c09d98840393b23ee
Bailey Guthrie <baileyguthrie@tamu.edu>
2019-04-10 02:34:28 +0000

    Input values now update with the dropdown

--- app/assets/javascripts/schedule.js
+++ app/assets/javascripts/schedule.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
   if (document.location.pathname == "/schedule") {
     $('select').on('change', function() {
-      alert(this.value);
-      alert(this);
+      var year_make_model = $('select#savedCars option:selected').text().split(" ");
+      console.log(year_make_model);
+      $('input#vehicleYear').val(year_make_model[0]);
+      $('input#vehicleMake').val(year_make_model[1]);
+      $('input#vehicleModel').val(year_make_model[2]);
+      $('input#vehicleVin').val(this.value);
     });
   }
 });
