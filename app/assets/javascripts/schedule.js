ea7c68f3fb0247d4ba7b7649199b0644c616e185
Bailey Guthrie <baileyguthrie@tamu.edu>
2019-04-11 01:33:59 +0000

    Fixed bug when using new car

--- app/assets/javascripts/schedule.js
+++ app/assets/javascripts/schedule.js
@@ -1,12 +1,18 @@
 document.addEventListener("turbolinks:load", function() {
   if (document.location.pathname == "/schedule") {
     $('select').on('change', function() {
-      var year_make_model = $('select#savedCars option:selected').text().split(" ");
-      console.log(year_make_model);
-      $('input#vehicleYear').val(year_make_model[0]);
-      $('input#vehicleMake').val(year_make_model[1]);
-      $('input#vehicleModel').val(year_make_model[2]);
-      $('input#vehicleVin').val(this.value);
+      if ($('select#savedCars option:selected').text() != "Use New Car") {
+        var year_make_model = $('select#savedCars option:selected').text().split(" ");
+          $('input#vehicleYear').val(year_make_model[0]);
+          $('input#vehicleMake').val(year_make_model[1]);
+          $('input#vehicleModel').val(year_make_model[2]);
+          $('input#vehicleVin').val(this.value);
+        } else {
+          $('input#vehicleYear').val("");
+          $('input#vehicleMake').val("");
+          $('input#vehicleModel').val("");
+          $('input#vehicleVin').val("");
+        }
     });
   }
 })
