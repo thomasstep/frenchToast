b805041038613cbac31a88bc9062caa6da0b511b
Thomas Step <tstep916@gmail.com>
2019-04-15 15:08:56 -0500

    Styled an obscure devise page. Messing with the devise mailer port.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "frenchtoast_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'https://thefrenchiesttoast.herokuapp.com', port: ENV["PORT"] }
+  config.action_mailer.default_url_options = { host: 'https://thefrenchiesttoast.herokuapp.com', port: ENV["DEVISE_MAILER_PORT"] }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
